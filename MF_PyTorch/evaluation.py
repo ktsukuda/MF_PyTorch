d7a0374c2a2ce550de8e24ab43950871bd1cdbf9
ktsukuda <k.tsukuda@aist.go.jp>
2020-07-28 12:10:38 +0900

    [add] compute metrics

---
+++ MF_PyTorch/evaluation.py
@@ -0,0 +1,76 @@
+import torch
+import math
+import pandas as pd
+
+
+def evaluate(model, evaluation_data, top_k):
+    model.eval()
+    metrics = Metrics(top_k)
+    with torch.no_grad():
+        pos_users, pos_items = evaluation_data[0], evaluation_data[1]
+        neg_users, neg_items = evaluation_data[2], evaluation_data[3]
+        pos_users = pos_users.to('cuda:0')
+        pos_items = pos_items.to('cuda:0')
+        neg_users = neg_users.to('cuda:0')
+        neg_items = neg_items.to('cuda:0')
+
+        pos_scores = model(pos_users, pos_items)
+        neg_scores = model(neg_users, neg_items)
+
+        pos_users = pos_users.to('cpu')
+        pos_items = pos_items.to('cpu')
+        pos_scores = pos_scores.to('cpu')
+        neg_users = neg_users.to('cpu')
+        neg_items = neg_items.to('cpu')
+        neg_scores = neg_scores.to('cpu')
+
+        metrics.subjects = [
+            pos_users.data.view(-1).tolist(),
+            pos_items.data.view(-1).tolist(),
+            pos_scores.data.view(-1).tolist(),
+            neg_users.data.view(-1).tolist(),
+            neg_items.data.view(-1).tolist(),
+            neg_scores.data.view(-1).tolist()]
+
+    hit_ratio = metrics.compute_hit_ratio()
+    ndcg = metrics.compute_ndcg()
+    return hit_ratio, ndcg
+
+
+class Metrics():
+
+    def __init__(self, top_k):
+        self._top_k = top_k
+        self._subjects = None
+
+    @property
+    def subjects(self):
+        return self._subjects
+
+    @subjects.setter
+    def subjects(self, subjects):
+        pos_users, pos_items, pos_scores = subjects[0], subjects[1], subjects[2]
+        neg_users, neg_items, neg_scores = subjects[3], subjects[4], subjects[5]
+        pos_df = pd.DataFrame({
+            'user': pos_users,
+            'pos_item': pos_items,
+            'pos_score': pos_scores})
+        full_df = pd.DataFrame({
+            'user': neg_users + pos_users,
+            'item': neg_items + pos_items,
+            'score': neg_scores + pos_scores})
+        full_df = pd.merge(full_df, pos_df, on=['user'], how='left')
+        full_df['rank'] = full_df.groupby('user')['score'].rank(method='first', ascending=False)
+        full_df.sort_values(['user', 'rank'], inplace=True)
+        self._subjects = full_df
+
+    def compute_hit_ratio(self):
+        top_k = self._subjects[self._subjects['rank']<=self._top_k]
+        pos_in_top_k = top_k[top_k['pos_item'] == top_k['item']]
+        return len(pos_in_top_k) / self._subjects['user'].nunique()
+
+    def compute_ndcg(self):
+        top_k = self._subjects[self._subjects['rank']<=self._top_k]
+        pos_in_top_k = top_k[top_k['pos_item'] == top_k['item']]
+        pos_in_top_k['ndcg'] = pos_in_top_k['rank'].apply(lambda x: math.log(2) / math.log(1 + x))
+        return pos_in_top_k['ndcg'].sum() / self._subjects['user'].nunique()
--- MF_PyTorch/main.py
+++ MF_PyTorch/main.py
@@ -3,6 +3,7 @@ import configparser
 from torch import nn, optim
 
 import data
+import evaluation
 from MF import MF
 
 
@@ -13,15 +14,16 @@ def main():
     data_splitter = data.DataSplitter()
     validation_data = data_splitter.make_evaluation_data('validation')
 
-    model = MF(data_splitter.n_user, data_splitter.n_item, 20)
+    model = MF(data_splitter.n_user, data_splitter.n_item, 8)
     print(model)
     model.to('cuda:0')
-    model.train()
 
     opt = optim.Adam(model.parameters(), lr=0.001, weight_decay=0)
     criterion = nn.BCELoss()
 
+    epoch_data = []
     for epoch in range(config.getint('MODEL', 'epoch')):
+        model.train()
         train_loader = data_splitter.make_train_loader(config.getint('MODEL', 'n_negative'), 1024)
         total_loss = 0
         for batch in tqdm.tqdm(train_loader):
@@ -35,6 +37,9 @@ def main():
             loss.backward()
             opt.step()
             total_loss += loss.item()
+        hit_ratio, ndcg = evaluation.evaluate(model, validation_data, config.getint('EVALUATION', 'top_k'))
+        epoch_data.append({'epoch': epoch, 'loss': total_loss, 'HR': hit_ratio, 'NDCG': ndcg})
+        print('[Epoch {}] Loss = {:.2f}, HR = {:.4f}, NDCG = {:.4f}'.format(epoch, total_loss, hit_ratio, ndcg))
 
 
 if __name__ == "__main__":
